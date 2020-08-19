36d3f5b0efe4b788ee6686b96f29baa4b4572619
CloverGit <w991593239@163.com>
2020-08-19 15:49:12 +0800

    Add notice

--- Plugin.php
+++ Plugin.php
@@ -13,6 +13,7 @@ class MemorialDay_Plugin implements Typecho_Plugin_Interface
     public static function activate()
     {
         Typecho_Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'website_set');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'notice_set');
         return _t('MemorialDay 插件已启用');
     }
 
@@ -26,11 +27,23 @@ class MemorialDay_Plugin implements Typecho_Plugin_Interface
         $days = new Typecho_Widget_Helper_Form_Element_Text(
             'days',
             null,
-            "0404,0512,0918,1213",
+            "0404,0512,0728,0918,1213",
             _t('日期：'),
             _t('日期使用英文逗号<code>,</code>分隔，可以自行增加删除日期；如果使用了CDN，请自行刷新缓存。')
         );
         $form->addInput($days->addRule('required', _t('日期为必填项')));
+       $notices = new Typecho_Widget_Helper_Form_Element_Text(
+            'notices',
+            null,
+            "清明时节，在此缅怀追思先人前辈，深切悼念2020年新冠肺炎疫情牺牲烈士和逝世同胞,
+            深切缅怀5·12汶川特大地震中的遇难同胞！,
+            深切缅怀7·28唐山特大地震中的遇难同胞！,
+            居安思危，警钟长鸣！岁月无疆，英雄不朽！勿忘国耻，共襄复兴！,
+            沉痛哀悼南京大屠杀中遇难同胞，我们永远不会忘记这一黑暗历史！",
+            _t('通知：'),
+            _t('通知使用英文逗号<code>,</code>分隔，数量必须与日期数相符，内容可以设置为空。')
+        );
+        $form->addInput($notices->addRule('required', _t('通知为必填项')));
     }
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
@@ -42,8 +55,19 @@ class MemorialDay_Plugin implements Typecho_Plugin_Interface
         $days = Typecho_Widget::widget('Widget_Options')->plugin('MemorialDay')->days;
         $day_arr = explode(",", $days);
         if (in_array(date('md'), $day_arr)) {
-            echo "<style type='text/css'>html{ filter: grayscale(100%); -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); -ms-filter: grayscale(100%); -o-filter: grayscale(100%); filter: url('data:image/svg+xml;utf8,#grayscale'); filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); -webkit-filter: grayscale(1);}</style>
-";
+            echo "<style type='text/css'>html{ filter: grayscale(100%); -webkit-filter: grayscale(100%); -moz-filter: grayscale(100%); -ms-filter: grayscale(100%); -o-filter: grayscale(100%); filter: url('data:image/svg+xml;utf8,#grayscale'); filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); -webkit-filter: grayscale(1);}</style>";
+        }
+    }
+
+    public static function notice_set()
+    {
+        $days = Typecho_Widget::widget('Widget_Options')->plugin('MemorialDay')->days;
+        $notices = Typecho_Widget::widget('Widget_Options')->plugin('MemorialDay')->notices;
+        $day_arr = explode(",", $days);
+        $notice_arr = explode(",", $notices);
+        if (in_array(date('md'), $day_arr)) {
+            $notice = $notice_arr[array_search(date('md'), $day_arr)];
+            echo "<marquee scrollamount=7 onmouseover=this.stop() onmouseout=this.start() bgcolor='#fffef9'><span style='color:#007947;'>{$notice}</span></marquee>";
         }
     }
 }
